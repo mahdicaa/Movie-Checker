eb3e5c78efe77aa4ddee97c1d480cb54774a3867
Mahdi Forootan <forotan1376@gmail.com>
2025-05-12 16:57:05 +0330

    adding some more details regarding each movies with their styles.

--- src/Rightcard.jsx
+++ src/Rightcard.jsx
@@ -12,6 +12,7 @@ export const Rightcard = ({ selectedMovieID, APIKEY }) => {
       );
       const currentMovie = await res.json();
       setCurrMovieData(() => currentMovie);
+      console.log(currentMovie);
     };
     movieIDfetcher();
   }, [selectedMovieID]);
@@ -19,7 +20,16 @@ export const Rightcard = ({ selectedMovieID, APIKEY }) => {
   return (
     <div className="flex flex-col items-center w-1/2  bg-gray-800 rounded-xl h-full overflow-hidden">
       {selectedMovieID && <MovieDetailCard curMovieData={curMovieData} />}
-      <StarRating />
+      <div className="p-10 w-9/10 text-white">
+        {selectedMovieID && (
+          <div className="p-5 bg-gray-700 rounded-2xl mb-5">
+            <StarRating />
+          </div>
+        )}
+        <i className="mt-5">{curMovieData.Plot}</i>
+        <p className="mt-5">{curMovieData.Actors}</p>
+        <p className="mt-5">Directed by {curMovieData.Director}</p>
+      </div>
     </div>
   );
 };
