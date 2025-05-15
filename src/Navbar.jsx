2627bd8361d4c7ecefa359fc7fc5189cba9ebf47
Mahdi Forootan <forotan1376@gmail.com>
2025-05-15 10:44:16 +0330

    fixing the issue of lacking of the number of results

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,10 +6,13 @@ export const Navbar = ({ onInputhandler, numberOfResults }) => {
         type="text"
         className="bg-purple-500 rounded-xl w-100 h-10 p-3"
         onChange={onInputhandler}
+        placeholder="Name of the movie..."
       />
-      <div className="font-bold text-white text-2xl">
-        Found {numberOfResults} top results
-      </div>
+      {numberOfResults && (
+        <div className="font-bold text-white text-2xl">
+          Found {numberOfResults} top results
+        </div>
+      )}
     </div>
   );
 };
